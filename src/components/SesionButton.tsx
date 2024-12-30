cf45bc4c69263f8025831fb0bc4afa71fe4d693d
Barbita <pabloezeromano@gmail.com>
2024-12-30 15:27:40 -0300

    Improve session management UI
    Signed-off-by: Barbita <pabloezeromano@gmail.com>

--- src/components/SesionButton.tsx
+++ src/components/SesionButton.tsx
@@ -1,26 +1,46 @@
 import { signIn, signOut, useSession } from 'next-auth/react';
 import { motion } from 'framer-motion';
+import Link from 'next/link';
+import { UserPlusIcon } from '../../public/icons';
 
 import '@/styles/button.css';
 
 export const SessionButton = () => {
   const { data: session } = useSession();
 
-  const buttonProps = session
-    ? { className: "secondary", onClick: () => signOut(), text: "Cerrar sesion", shadowColor: "#946b4d" }
-    : { className: "primary", onClick: () => signIn(), text: "Iniciar sesion", shadowColor: "#ff6347" };
+  if (session) {
+    return (
+      <motion.button
+        className="secondary"
+        onClick={() => signOut()}
+        whileHover={{
+          scale: 1.1,
+          boxShadow: '0 0 8px #946b4d',
+          textShadow: '0 0 8px #946b4d',
+        }}
+      >
+        <span className="truncate">Cerrar sesión</span>
+      </motion.button>
+    );
+  }
 
   return (
-    <motion.button
-      className={buttonProps.className}
-      onClick={buttonProps.onClick}
-      whileHover={{
-        scale: 1.1,
-        boxShadow: `0 0 8px ${buttonProps.shadowColor}`,
-        textShadow: `0 0 8px ${buttonProps.shadowColor}`,
-      }}
-    >
-      <span className="truncate">{buttonProps.text}</span>
-    </motion.button>
+    <>
+      <motion.button
+        className="primary"
+        onClick={() => signIn()}
+        whileHover={{
+          scale: 1.1,
+          boxShadow: '0 0 8px #6366f1',
+          textShadow: '0 0 8px #6366f1',
+        }}
+      >
+        <span className="truncate">Iniciar sesión</span>
+      </motion.button>
+      <Link href="/auth/sign-up" className="navigation-item flex-1">
+        <UserPlusIcon className="w-6 h-6" />
+        <span>Registrarse</span>
+      </Link>
+    </>
   );
 };
